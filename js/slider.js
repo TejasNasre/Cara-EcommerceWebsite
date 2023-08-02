f39790be2ad46e22c1ed9fdb0f9dbdf159df68a0
Tejas Nasre <113225478+TejasNasre@users.noreply.github.com>
2023-08-02 21:45:34 +0530

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,15 @@
+var mainImg = document.getElementById("mainImg");
+var smallImg = document.getElementsByClassName("small-img");
+
+smallImg[0].addEventListener("click", () =>{
+    mainImg.src = smallImg[0].src;
+})
+smallImg[1].addEventListener("click", () =>{
+    mainImg.src = smallImg[1].src;
+})
+smallImg[2].addEventListener("click", () =>{
+    mainImg.src = smallImg[2].src;
+})
+smallImg[3].addEventListener("click", () =>{
+    mainImg.src = smallImg[3].src;
+})
